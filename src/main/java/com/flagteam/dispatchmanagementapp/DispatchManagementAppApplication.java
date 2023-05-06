f76459c79657df5aac533081ecb651c11b3975b0
Alex Li <islicy@outlook.com>
2023-05-06 18:59:01 -0400

    Initial commit

---
+++ src/main/java/com/flagteam/dispatchmanagementapp/DispatchManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.flagteam.dispatchmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispatchManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flagteam/dispatchmanagementapp/DispatchManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flagteam.dispatchmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
