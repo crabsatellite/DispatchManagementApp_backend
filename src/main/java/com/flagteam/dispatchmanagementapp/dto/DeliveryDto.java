447500ce9a86affc6abfcc08987d69a29bc2632f
Alex Li <islicy@outlook.com>
2023-05-13 21:32:54 -0400

    Update DeliveryDto

--- src/main/java/com/flagteam/dispatchmanagementapp/dto/DeliveryDto.java
+++ src/main/java/com/flagteam/dispatchmanagementapp/dto/DeliveryDto.java
@@ -1,9 +1,32 @@
 package com.flagteam.dispatchmanagementapp.dto;
 
+import java.util.Set;
+import java.util.UUID;
+
 public class DeliveryDto {
-    // all fields from DeliveryItem
+    private UUID id;
+    private int quantity;
     private Double courierLastPositionLat;
     private Double courierLastPositionLng;
+    private UUID deliveryInfoId;
+    private Set<UUID> itemIds;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    // getters and setters for quantity
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
     public Double getCourierLastPositionLat() {
         return courierLastPositionLat;
@@ -19,5 +42,23 @@ public class DeliveryDto {
 
     public void setCourierLastPositionLng(Double courierLastPositionLng) {
         this.courierLastPositionLng = courierLastPositionLng;
+    }
+
+    // getters and setters for deliveryInfoId
+    public UUID getDeliveryInfoId() {
+        return deliveryInfoId;
+    }
+
+    public void setDeliveryInfoId(UUID deliveryInfoId) {
+        this.deliveryInfoId = deliveryInfoId;
+    }
+
+    // getters and setters for itemIds
+    public Set<UUID> getItemIds() {
+        return itemIds;
+    }
+
+    public void setItemIds(Set<UUID> itemIds) {
+        this.itemIds = itemIds;
     }
 }
